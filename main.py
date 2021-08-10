7748da25373577a3bef305c4886bae2d6675b171
MrRavenMan <cami-esf@hotmail.com>
2021-08-10 17:05:17 +0200

    Added status messages

--- main.py
+++ main.py
@@ -26,6 +26,25 @@ client.add_cog(MemberWatch(client, config['MemberWatch']))
 @client.event
 async def on_ready():
     print(f'{config["General"]["bot_name"]} has logged in as {client.user}')
+
+    # Add custom status
+    if (int(config["General"]["status"]) == 1):
+        print(f"Using status 1: Playing {config['General']['status_message']}")
+        await client.change_presence(activity=discord.Game(name=config["General"]["status_message"]))
+    elif (int(config["General"]["status"]) == 2):
+        print(f"Using status 2: Streaming {config['General']['status_message']}. Link: {config['General']['status_streaming_url']}")
+        await client.change_presence(activity=discord.Streaming(name=config["General"]["status_message"], url=config["General"]["status_streaming_url"]))
+    elif (int(config["General"]["status"]) == 3):
+        print(f"Using status 3: Watching {config['General']['status_message']}")
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=config["General"]["status_message"]))
+    elif (int(config["General"]["status"]) == 4):
+        print(f"Using status 4: Competing in {config['General']['status_message']}")
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name=config["General"]["status_message"]))
+    elif (int(config["General"]["status"]) == 5):
+        print(f"Using status 5: Listening to {config['General']['status_message']}")
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=config["General"]["status_message"]))
+
+
     DiscordComponents(client)
 
 
